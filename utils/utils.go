f16dc24b140444e0e1a8ac0b03de61dbd785b0e9
narutomanchester <narutomanchester@gmail.com>
2019-03-01 18:35:56 +0700

    update utils

--- utils/utils.go
+++ utils/utils.go
@@ -7,7 +7,7 @@ import (
 	. "github.com/hongsongp97/tickethunter_server/model"
 )
 
-func respondWithError(w http.ResponseWriter, code int, msg string) {
+func RespondWithError(w http.ResponseWriter, code int, msg string) {
 	response := ResponseJson{Status: code, Message: msg}
 	json.NewEncoder(w).Encode(response)
 	// respondWithJson(w, code, map[string]string{"error": msg})
