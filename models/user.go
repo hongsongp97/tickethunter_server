0682c23d78717dcd3498dcbc3e35c4038da04f01
SonPH <hongsongp97@gmail.com>
2019-03-03 10:52:58 +0700

    create event DAO

--- models/user.go
+++ models/user.go
@@ -1,7 +1,7 @@
 package models
 
 type User struct {
-	ID           string `bson:"_id,omitempty" json:"id"`
+	Id           string `bson:"_id,omitempty" json:"id"`
 	FirstName    string `bson:"first_name" json:"first_name"`
 	LastName     string `bson:"last_name" json:"last_name"`
 	UserName     string `bson:"user_name" json:"user_name"`
